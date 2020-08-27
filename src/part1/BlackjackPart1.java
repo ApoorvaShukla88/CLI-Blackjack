a0258a6319c292eb5b8384fac203d19e09f3fac2
koastiebratt@gmail.com <you@example.com>
2020-08-27 19:22:26 -0400

    Added  try catch in quit for user input

--- src/part1/BlackjackPart1.java
+++ src/part1/BlackjackPart1.java
@@ -1,5 +1,6 @@
 package part1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class BlackjackPart1 {
@@ -12,58 +13,70 @@ public class BlackjackPart1 {
 	
 	public static void main(String[] args) {
 		
+		Continue();
 		Scanner input = new Scanner(System.in);
-		
 		boolean WannaPlay = true;
-		
-		
 		
 			// Intialize the card
 			for(int i = 0; i < deck.length; i++) deck[i] = i;	
 		
 		do {	
 			
+			//Reset to 0 each itteration
+			int dealerScore = 0;
+			int playerScore = 0;
+			
+			
 			// Shuffle the cards
-						for( int i = 0; i < deck.length; i++) {
-							int index = (int) (Math.random() * deck.length);
-							int temp = deck[i];
-							deck[i] = deck[index];
-							deck[index] = temp;
-							
-							
-						}		
-				
-				
+				for( int i = 0; i < deck.length; i++) {
+					int index = (int) (Math.random() * deck.length);
+					int temp = deck[i];
+					deck[i] = deck[index];
+					deck[index] = temp;
+				}		
 
 				
-				
-				int dealerScore = 0;
-				int playerScore = 0;
-				
-				// distrobution of cards to player and dealer
+				// Distribution of cards to player and dealer
 				for(int i = 0; i < 4; i++) {
+					//Dealer, Player, Dealer, Player
 					if (i% 2 == 0) {
+						
 						playerScore += getScore(deck[i]);
+						
 					}else {
+						
 						dealerScore += getScore(deck[i]);
 					}
 				}
+				
 				System.out.println("Player Score  is " + playerScore);
 				System.out.println("Dealer Score  is " + dealerScore);
 				
 				if (!checkWinner(playerScore, dealerScore)) {
 					int j = 4;
+					
 					Scanner hitOrStand = new Scanner(System.in);
+					
 					System.out.println("Enter h to HIT or s to STAND : ");
+					
 					char userChoice = hitOrStand.next().charAt(0);
-					while(userChoice == 'h') {
+					
+					while(userChoice == 'h' || userChoice =='H') {
+						
 						playerScore += getScore(deck[j]);
+						
 						if (!checkWinner(playerScore, dealerScore)) {
+							
 							j++;
+							
 							dealerScore += getScore(deck[j]);
+							
 							if (!checkWinner(playerScore, dealerScore)) {
+								
 								j++;
+								
 							userChoice = hitOrStand.next().charAt(0);}
+							
 						} else userChoice = 's';
 //						if (checkWinner(playerScore, dealerScore)) {
 //							WannaPlay = Continue();
@@ -89,9 +102,12 @@ public class BlackjackPart1 {
 	
 	
 	public static int getScore(int cardIndex) {
+		
 		String suit = suits[deck[cardIndex] / 13];
 		String rank = ranks[deck[cardIndex] % 13];
+		
 		int result = Integer.parseInt(rank);
+		
         switch (Integer.parseInt(rank))
         {
             case 11:
@@ -106,8 +122,11 @@ public class BlackjackPart1 {
 	public static boolean checkWinner(int playerScore, int dealerScore) {
 		
 		if (playerScore == 21 || dealerScore > 21) {
+			
 			System.out.println("Player Wins");
+			
 			return true;
+			
 		} else if (dealerScore ==21 || playerScore > 21) {
 			System.out.println("Dealer Wins");
 			return true;
@@ -120,42 +139,40 @@ public class BlackjackPart1 {
 	public static boolean Continue() {
 		
 		Scanner temp_input = new Scanner(System.in);
-		
-		boolean temp_bool = false;
 		char UserInput;
-
-		System.out.println();
-		System.out.print("Would you like to play again? (y / n) : ");
-		
-		UserInput = temp_input.next().charAt(0);
-		
-//		System.out.println(UserInput);
-//		System.out.println((UserInput == 'y') + "  " + (UserInput == 'Y'));
-		
-		if(UserInput != 'Y' && UserInput != 'y' && UserInput != 'N' && UserInput != 'n') {
+		boolean Continue = true;
+	
+		try {
 			
-			System.out.print("Invalid response detected. Please try again...... \n\n");
+			System.out.println();
+			System.out.print("Would you like to play again? (y / n) : ");
+			UserInput = temp_input.next().charAt(0);
 			
-			Continue();
+			if(UserInput == 'Y' || UserInput == 'y') {
+				
+				Continue = true;
+				
+			}else if(UserInput == 'n' || UserInput == 'N') {
+				
+				Continue = false;
+			}else{
+				
+				System.out.print("Invalid Response detected.... Please Try Again");
+				Continue();
+			}
 			
-		}else if(UserInput == 'y' || UserInput == 'Y') {
+		}catch(InputMismatchException e) {
 			
-			temp_bool = true;
+			System.out.println("Please either y or n");
+			System.out.println();
+			temp_input.nextLine();
 			
-		}else if (UserInput == 'n' || UserInput == 'N') {
+		}finally{
 			
-			temp_bool =  false;
-			
-		}else{
-			
-			System.out.println("Fatal Error... Please Try again.....");
-			
-			Continue();
+			temp_input.close();
 		}
 		
-		
-		
-		return temp_bool;
+		return Continue;
 	}
 		
 }
