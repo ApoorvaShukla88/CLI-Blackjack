e74505eddff80cff342c20734c1bdbc682223e0a
koastiebratt@gmail.com <you@example.com>
2020-08-28 00:19:33 -0400

    Very Tired. Looks like alot. Try to follow in steps

--- src/part1/BlackjackPart1.java
+++ src/part1/BlackjackPart1.java
@@ -8,13 +8,12 @@ public class BlackjackPart1 {
 		
 		int[] new_deck = new int[52];
 		
-		
-		
 		//Create A Deck
 		new_deck = NewDeck.NewDeck();
 		
 		Scanner input = new Scanner(System.in);
 		boolean WannaPlay = true;
+		boolean DelearBlackJackCheck = false;
 		
 			// Initialize the card
 				
@@ -27,63 +26,45 @@ public class BlackjackPart1 {
 			
 			//Creates instance of new deck
 			new_deck = NewDeck.NewDeck();
-				
-
-				
-				// Distribution of cards to player and dealer
-				for(int i = 0; i < 4; i++) {
-					//Dealer, Player, Dealer, Player
-					if (i% 2 == 0) {
-						
-						playerScore += Score.getScore(new_deck[i]);
-						
-					}else {
-						
-						dealerScore += Score.getScore(new_deck[i]);
-					}
+		
+			// Distribution of cards to player and dealer
+			for(int i = 0; i < 4; i++) {
+				//Dealer, Player, Dealer, Player
+				if (i% 2 == 0) {
+					
+					playerScore += Score.getScore(new_deck[i]);
+					
+				}else {
+					
+					dealerScore += Score.getScore(new_deck[i]);
 				}
-				
-				System.out.println("Player Score  is " + playerScore);
-				System.out.println("Dealer Score  is " + dealerScore);
-				
-				if (!checkWinner(playerScore, dealerScore)) {
-					int j = 4;
-					
-					Scanner hitOrStand = new Scanner(System.in);
-					
-					System.out.println("Enter h to HIT or s to STAND : ");
-					
-					char userChoice = hitOrStand.next().charAt(0);
-					
-					while(userChoice == 'h' || userChoice =='H') {
-						
-						playerScore += Score.getScore(new_deck[j]);
-						
-						if (!checkWinner(playerScore, dealerScore)) {
-							
-							j++;
-							
-							dealerScore += Score.getScore(new_deck[j]);
-							
-							if (!checkWinner(playerScore, dealerScore)) {
-								
-								j++;
-								
-							userChoice = hitOrStand.next().charAt(0);}
-							
-						} else userChoice = 's';
-//						if (checkWinner(playerScore, dealerScore)) {
-//							WannaPlay = Continue();
-//						} else {
-//							break;
-//						}
-					}
-				}
-				
-				
-				//Calls Continue Method
-				WannaPlay = Continue.Continue();	
-				
+			}
+			
+//			System.out.println(playerScore);
+//			System.out.println(dealerScore);
+			
+			//Check Dealer Black Jack before play starts after dealing
+			//If dealer has A + 10/J/Q/K they auto win if player doesn't have it as well
+			boolean auto_lost = Winner.BlackJackCheck(playerScore, dealerScore);
+			
+			System.out.println(auto_lost);
+			//Automatically reset the game if plater wants to continue;
+			//After losing to a blackjack
+			if(auto_lost) {
+				WannaPlay = Continue.Continue();
+				if(WannaPlay){main(args);}	
+			}
+			
+			System.out.println("Player Score  is " + playerScore);
+			System.out.println("Dealer Score  is " + dealerScore);
+			System.out.println();
+			
+			Hit.hit(playerScore, dealerScore, auto_lost, new_deck);
+			
+			
+			//Calls Continue Method
+			WannaPlay = Continue.Continue();	
+			
 				
 			}while(WannaPlay == true);
 		
@@ -93,27 +74,14 @@ public class BlackjackPart1 {
 		
 		input.close();
 	}
-	
-	
-	
-	
-	public static boolean checkWinner(int playerScore, int dealerScore) {
-		
-		if (playerScore == 21 || dealerScore > 21) {
-			
-			System.out.println("Player Wins");
-			
-			return true;
-			
-		} else if (dealerScore ==21 || playerScore > 21) {
-			System.out.println("Dealer Wins");
-			return true;
-		} else {
-			return false;
-		}
-		
-	}
-	//Continue class
-	
-		
 }
+
+
+
+
+
+
+
+
+
+
